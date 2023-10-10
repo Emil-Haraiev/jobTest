3975ee844516676d5ac00ca3331bb0be5d7fc953
Emil <e.garaev.dg@gmail.com>
2023-10-10 15:26:53 +0300

    contact

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -23,83 +23,87 @@ const Contact = () => {
 
     return (
         <div className="contact-container">
-            <h2>Contact us</h2>
-            <div className="contact-methods">
-                <div
-                    className={`contact-method ${contactMethod === 'email' ? 'active' : ''}`}
-                    onClick={() => handleContactMethodChange('email')}
-                >
-                    E-mail
+            <h2 className='contact-title'>Contact us</h2>
+            <div className="form-wrap">
+                <div className="contact-methods">
+                    <div
+                        className={`contact-method ${contactMethod === 'email' ? 'active' : ''}`}
+                        onClick={() => handleContactMethodChange('email')}
+                    >
+                        E-mail
+                    </div>
+                    <div
+                        className={`contact-method ${contactMethod === 'telegram' ? 'active' : ''}`}
+                        onClick={() => handleContactMethodChange('telegram')}
+                    >
+                        Telegram
+                    </div>
+                    <div
+                        className={`contact-method ${contactMethod === 'whatsapp' ? 'active' : ''}`}
+                        onClick={() => handleContactMethodChange('whatsapp')}
+                    >
+                        WhatsApp
+                    </div>
+                    <div
+                        className={`contact-method ${contactMethod === 'viber' ? 'active' : ''}`}
+                        onClick={() => handleContactMethodChange('viber')}
+                    >
+                        Viber
+                    </div>
                 </div>
-                <div
-                    className={`contact-method ${contactMethod === 'telegram' ? 'active' : ''}`}
-                    onClick={() => handleContactMethodChange('telegram')}
-                >
-                    Telegram
-                </div>
-                <div
-                    className={`contact-method ${contactMethod === 'whatsapp' ? 'active' : ''}`}
-                    onClick={() => handleContactMethodChange('whatsapp')}
-                >
-                    WhatsApp
-                </div>
-                <div
-                    className={`contact-method ${contactMethod === 'viber' ? 'active' : ''}`}
-                    onClick={() => handleContactMethodChange('viber')}
-                >
-                    Viber
-                </div>
+                <form className="contact-form">
+                    <input
+                        type="text"
+                        name="name"
+                        placeholder="Name"
+                        required={true}
+                        value={formData.name}
+                        onChange={handleInputChange}
+                    />
+                    <input
+                        type="text"
+                        name="email"
+                        placeholder="E-mail"
+                        required={true}
+                        value={formData.email}
+                        onChange={handleInputChange}
+                    />
+                    {contactMethod === 'telegram' && (
+                        <input
+                            type="text"
+                            name="telegram"
+                            placeholder="Telegram username"
+                            value={formData.telegram}
+                            onChange={handleInputChange}
+                        />
+                    )}
+                    {contactMethod === 'whatsapp' && (
+                        <input
+                            type="text"
+                            name="whatsapp"
+                            placeholder="WhatsApp number"
+                            value={formData.whatsapp}
+                            onChange={handleInputChange}
+                        />
+                    )}
+                    {contactMethod === 'viber' && (
+                        <input
+                            type="text"
+                            name="viber"
+                            placeholder="Viber number"
+                            value={formData.viber}
+                            onChange={handleInputChange}
+                        />
+                    )}
+                    <textarea
+                        name="message"
+                        placeholder="Message"
+                        value={formData.message}
+                        onChange={handleInputChange}
+                    ></textarea>
+                    <button type="submit">Send</button>
+                </form>
             </div>
-            <form className="contact-form">
-                <input
-                    type="text"
-                    name="name"
-                    placeholder="Name"
-                    value={formData.name}
-                    onChange={handleInputChange}
-                />
-                <input
-                    type="text"
-                    name="email"
-                    placeholder="E-mail"
-                    value={formData.email}
-                    onChange={handleInputChange}
-                />
-                {contactMethod === 'telegram' && (
-                    <input
-                        type="text"
-                        name="telegram"
-                        placeholder="Telegram username"
-                        value={formData.telegram}
-                        onChange={handleInputChange}
-                    />
-                )}
-                {contactMethod === 'whatsapp' && (
-                    <input
-                        type="text"
-                        name="whatsapp"
-                        placeholder="WhatsApp number"
-                        value={formData.whatsapp}
-                        onChange={handleInputChange}
-                    />
-                )}
-                {contactMethod === 'viber' && (
-                    <input
-                        type="text"
-                        name="viber"
-                        placeholder="Viber number"
-                        value={formData.viber}
-                        onChange={handleInputChange}
-                    />
-                )}
-                <textarea
-                    name="message"
-                    placeholder="Message"
-                    value={formData.message}
-                    onChange={handleInputChange}
-                ></textarea>
-                <button type="submit">Send</button>
-            </form>
         </div>
     );
 };
