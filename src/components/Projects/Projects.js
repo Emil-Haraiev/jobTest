b38bff5cf04fc60e38a1ba36b18348aec91a6c5d
Emil <e.garaev.dg@gmail.com>
2023-10-11 18:55:43 +0300

    slides

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -12,9 +12,10 @@ SwiperCore.use([]);
 const Projects = () => {
     return (
         <div className="project-container">
+            <h1 className="project-title">/ Projects</h1>
             <Swiper
                 slidesPerView={1}
-                spaceBetween={20}
+                spaceBetween={10}
                 centeredSlides
                 grabCursor
                 loop={true}
@@ -22,22 +23,35 @@ const Projects = () => {
                 <SwiperSlide className="project-slide">
                     <img className='slide-img' src={slide1} alt="Slide 1" />
                     <div className="slide-content">
-                        <h3>Slide 1</h3>
-                        <p>Description for Slide 1</p>
+                        <h3>Marketplace</h3>
+                        <p>
+                            Volstiny production - Marketplace for a Designers agency. Powerful
+                            and creative website for powerfull creators. Unique design, a lot of animations and
+                            internal marketplace - website is faster than a Forest Gump and ready for a SEO ad
+                            campaigns.
+                        </p>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide className="project-slide">
                     <img className='slide-img' src={slide2} alt="Slide 2" />
                     <div className="slide-content">
-                        <h3>Slide 2</h3>
-                        <p>Description for Slide 2</p>
+                        <h3>Web-Service</h3>
+                        <p>
+                            Lernitlive - webservice for coaches and experts where they can teach
+                            online. In easy words - it is a marketplace with some more functionality to pass courses
+                            and take classes. You can look throug the courses, choose your coach, according to his
+                            rate and reviews. Functionality: you can register,buy classes, chat with your coach and
+                            leave reviews.
+                        </p>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide className="project-slide">
                     <img className='slide-img' src={slide3} alt="Slide 3" />
                     <div className="slide-content">
-                        <h3>Slide 3</h3>
-                        <p>Description for Slide 3</p>
+                        <h3>Delivery Service</h3>
+                        <p>outsourcing company
+                            software development it agency
+                            outsourcing it company</p>
                     </div>
                 </SwiperSlide>
             </Swiper>
